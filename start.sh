1a604b759870ae01936791d287f93290f3370ba4
linfeng <scstlinfeng@aliyun.com>
2024-12-24 14:45:49 +0800

    init

--- start.sh
+++ start.sh
@@ -17,7 +17,7 @@ fi
 # 使用 nohup 后台启动应用并将日志输出到指定文件
 nohup java -Djava.security.egd=file:/dev/./urandom \
            -Dspring.profiles.active=prod \
-           -XX:+HeapDumpOnOutOfMemoryError -XX:+UseZGC -Xms1024m -Xmx2048m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256m \
+           -XX:+HeapDumpOnOutOfMemoryError -XX:+UseZGC -Xms1024m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256m \
            -jar $APP_NAME > $LOG_FILE 2>&1 &
 
 echo "应用已启动，日志输出到 $LOG_FILE"
